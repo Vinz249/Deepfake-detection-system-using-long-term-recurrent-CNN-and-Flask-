3f4806d04cba2e50c1372dd9ec92127b7bf57967
Vinz249 <sharmavinayak4u@gmail.com>
2023-12-03 20:17:04 +0530

    Made formatting changes

--- app/torch_utils.py
+++ app/torch_utils.py
@@ -40,6 +40,119 @@ class MyModel(nn.Module):
 model = MyModel(2)
 
 device = torch.device('cpu')
-PATH = "C:\\Users\\ANKUR SINGH\\Desktop\\PROJECTS\\Deepfake Detector\\Deepfake-detection-system-using-long-term-recurrent-CNN-and-Flask-\\app\\model(1).pth"
+PATH = "D:\docs\7th sem\major project 1\VS_Code\model(1) - Copy.pth"
 model.load_state_dict(torch.load(PATH, map_location=device))
 model.eval()
+
+
+
+
+
+#create only face
+
+def frame_extract(path):
+  vidObj = cv2.VideoCapture(path)
+  success = 1
+  while success:
+      success, image = vidObj.read()
+      if success:
+          yield image
+
+
+def create_face_video(input_video_path, output_video_path):
+    frames = []
+    out = cv2.VideoWriter(output_video_path, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 30, (112, 112))
+
+    # Initialize face detector from dlib
+    face_detector = dlib.get_frontal_face_detector()
+
+    for idx, frame in enumerate(frame_extract(input_video_path)):
+        if idx <= 150:
+            frames.append(frame)
+            if len(frames) == 4:
+                # Convert frames to grayscale for face detection
+                gray_frames = [cv2.cvtColor(f, cv2.COLOR_BGR2GRAY) for f in frames]
+
+                # Detect faces using dlib face detector
+                faces = [face_detector(gray_frame) for gray_frame in gray_frames]
+
+                for i, face in enumerate(faces):
+                    if face:
+                        top, right, bottom, left = (face[0].top(), face[0].right(), face[0].bottom(), face[0].left())
+                        try:
+                            out.write(cv2.resize(frames[i][top:bottom, left:right, :], (112, 112)))
+                        except:
+                            pass
+                frames = []
+
+    out.release()
+
+
+#preprocess
+def get_data_transforms(im_size=112):
+    mean = [0.485, 0.456, 0.406]
+    std = [0.229, 0.224, 0.225]
+
+    train_transforms = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.Resize((im_size, im_size)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+    ])
+
+    return train_transforms
+
+
+def preprocess(random_video_path):
+
+    video_frames = []
+
+    cap = cv2.VideoCapture(random_video_path)
+    while True:
+        success, frame = cap.read()
+        if not success:
+            break
+        video_frames.append(frame)
+
+    cap.release()
+
+    # Preprocess the video frames
+    data_transform = get_data_transforms()
+    processed_frames = [data_transform(frame) for frame in video_frames]
+    video_tensor = torch.stack(processed_frames)
+    # Ensure that the sequence_length parameter matches the one used during training
+    sequence_length = 60
+    video_tensor = video_tensor[:sequence_length]
+    return video_tensor
+
+
+def predict(video_tensor):
+    # Run inference
+    if torch.cuda.is_available():
+        video_tensor = video_tensor.cuda()
+
+    predicted_labels = []
+
+    with torch.no_grad():
+        for frame_tensor in video_tensor:
+            # Unsqueeze to add the batch and sequence dimensions
+            frame_tensor = frame_tensor.unsqueeze(0).unsqueeze(1)
+            _, predictions = model(frame_tensor)
+            _, predicted_label = torch.max(predictions, 1)
+            predicted_label = predicted_label.item()
+
+            # Accumulate predicted labels
+            predicted_labels.append(predicted_label)
+            return predicted_labels   
+
+
+def output_predict(predicted_labels):
+    # Calculate overall accuracy
+    overall_accuracy = (predicted_labels.count(1) / len(predicted_labels)) * 100.0
+
+    # Determine the overall predicted label
+    overall_predicted_label = 1 if overall_accuracy > 50 else 0
+
+    # Print or use the results
+    print(f"Overall Accuracy: {overall_accuracy:.2f}%")
+    print(f"Overall Prediction: {'REAL' if overall_predicted_label == 1 else 'FAKE'}")
