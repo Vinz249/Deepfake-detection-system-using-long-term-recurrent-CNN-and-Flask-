8b90055c8c380a3381b3c19e6f6264beddfc67cc
Vinz249 <sharmavinayak4u@gmail.com>
2023-12-05 00:01:42 +0530

    Removed errors from app

--- app/torch_utils.py
+++ app/torch_utils.py
@@ -10,7 +10,7 @@ import pandas as pd
 import glob
 import matplotlib.pyplot as plt
 import dlib
-
+import traceback
 # define model class
 
 from torch import nn
@@ -41,7 +41,7 @@ class MyModel(nn.Module):
 model = MyModel(2)
 
 device = torch.device('cpu')
-PATH = "D:/docs/7th sem/major project 1/VS_Code/model(1).pth"
+PATH = "D:/docs/7th sem/major project 1/VS_Code/model(1_seq_10_7epoch).pth"
 model.load_state_dict(torch.load(PATH, map_location=device))
 model.eval()
 
@@ -52,12 +52,16 @@ model.eval()
 #create only face
 
 def frame_extract(path):
-  vidObj = cv2.VideoCapture(path)
-  success = 1
-  while success:
-      success, image = vidObj.read()
-      if success:
-          yield image
+    vidObj = cv2.VideoCapture(str(path))  # Ensure that path is passed as a string
+    success = True
+    frame_count = 0  # Add this line to count frames
+    while success:
+        success, image = vidObj.read()
+        if success:
+            frame_count += 1  # Increment frame count
+            yield image
+
+    print(f"Number of frames captured: {frame_count}")
 
 
 def create_face_video(input_video_path, output_video_path):
@@ -105,26 +109,61 @@ def get_data_transforms(im_size=112):
 
 
 def preprocess(random_video_path):
-
     video_frames = []
-
     cap = cv2.VideoCapture(random_video_path)
-    while True:
-        success, frame = cap.read()
-        if not success:
-            break
-        video_frames.append(frame)
 
-    cap.release()
+    # Check if the video capture is successful
+    if not cap.isOpened():
+        print(f"Error: Unable to open video file: {random_video_path}")
+        return None
 
-    # Preprocess the video frames
-    data_transform = get_data_transforms()
-    processed_frames = [data_transform(frame) for frame in video_frames]
-    video_tensor = torch.stack(processed_frames)
-    # Ensure that the sequence_length parameter matches the one used during training
-    sequence_length = 10
-    video_tensor = video_tensor[:sequence_length]
-    return video_tensor
+    try:
+        while True:
+            success, frame = cap.read()
+            if not success:
+                break
+            if frame is not None and frame.size != 0:  # Check for non-empty frames
+                video_frames.append(frame)
+
+        # Check if any frames were captured
+        if not video_frames:
+            print("Error: No frames were captured from the video.")
+            return None
+
+        print(f"Number of frames captured: {len(video_frames)}")
+
+        # Preprocess the video frames
+        data_transform = get_data_transforms()
+        processed_frames = [data_transform(frame) for frame in video_frames]
+
+        # Check if the processed frames list is empty
+        if not processed_frames:
+            print("Error: No frames were successfully processed.")
+            return None
+
+        # Check the shape of the first processed frame
+        print(f"Shape of the first processed frame: {processed_frames[0].shape}")
+
+        # Stack the processed frames into a video tensor
+        video_tensor = torch.stack(processed_frames)
+
+        # Ensure that the sequence_length parameter matches the one used during training
+        sequence_length = 10
+        video_tensor = video_tensor[:sequence_length]
+
+        # Check the shape of the resulting video tensor
+        print(f"Shape of the video tensor: {video_tensor.shape}")
+
+        return video_tensor
+
+    finally:
+        cap.release()
+
+    
+
+
+
+
 
 
 def predict(video_tensor):
@@ -135,49 +174,55 @@ def predict(video_tensor):
     predicted_labels = []
 
     with torch.no_grad():
-        for frame_tensor in video_tensor:
-            # Unsqueeze to add the batch and sequence dimensions
-            frame_tensor = frame_tensor.unsqueeze(0).unsqueeze(1)
-            _, predictions = model(frame_tensor)
-            _, predicted_label = torch.max(predictions, 1)
-            predicted_label = predicted_label.item()
+        try:
+            for frame_tensor in video_tensor:
+                # Unsqueeze to add the batch and sequence dimensions
+                frame_tensor = frame_tensor.unsqueeze(0).unsqueeze(1)
+                _, predictions = model(frame_tensor)
+                _, predicted_label = torch.max(predictions, 1)
+                predicted_label = predicted_label.item()
 
-            # Accumulate predicted labels
-            predicted_labels.append(predicted_label)
-    return predicted_labels   
+                # Accumulate predicted labels
+                predicted_labels.append(predicted_label)
+
+        except Exception as e:
+            traceback.print_exc()
+            print(f"Error during prediction: {str(e)}")
+            return []  # Return an empty list if there's an issue during prediction
+
+    return predicted_labels
+ 
 
 
 def output_predict(predicted_labels):
+    if not predicted_labels:
+        return "No predictions available."
+
     # Calculate the number of 'REAL' and 'FAKE' predictions
     num_real = predicted_labels.count(1)
     num_fake = len(predicted_labels) - num_real
+
+    # Check if the denominator is zero
+    if len(predicted_labels) == 0:
+        return "Error: Division by zero."
 
     # Calculate accuracies for 'REAL' and 'FAKE'
     real_accuracy = (num_real / len(predicted_labels)) * 100.0
     fake_accuracy = (num_fake / len(predicted_labels)) * 100.0
 
-    
-
     # Determine the overall predicted label
     if real_accuracy > fake_accuracy:
-      print(f"THE VIDEO IS REAL    Accuracy: {real_accuracy:.2f}%")
+        result = f"THE VIDEO IS REAL    Accuracy: {real_accuracy:.2f}%"
     else:
-      print(f"THE VIDEO IS FAKE    Accuracy: {100-fake_accuracy:.2f}%")
-   
+        result = f"THE VIDEO IS FAKE    Accuracy: {fake_accuracy:.2f}%"
+
+    return result
+
+
     
 
 
 
 
 
-    """
-         
-        except:
-            return jsonify({'error': 'error during prediction'})
-
-    return render_template('index.html')
-
-
-app.run(debug=True)
-
-"""
+ 
